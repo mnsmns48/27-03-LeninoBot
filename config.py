4838727e66eb17931648d3059060ab6dc431e546
mnsmns48 <getzboy@list.ru>
2023-03-09 17:13:58 +0300

    first work version without throttle filter

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from environs import Env
 @dataclass
 class TgBot:
     bot_token: str
+    admin_id: int
 
 
 @dataclass
@@ -18,5 +19,6 @@ def load_config(path: str = None):
 
     return Config(
         tg_bot=TgBot(
-            bot_token=env.str("BOT_TOKEN")
+            bot_token=env.str("BOT_TOKEN"),
+            admin_id=env.int("ADMIN_ID")
         ))
